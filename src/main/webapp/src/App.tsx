8953f9183389047fcac564046d39a36e4713d650
Renaud Pelissier <renaud.pelissier@metroscope.tech>
2021-12-04 20:43:26 +0100

    Layout

--- src/main/webapp/src/App.tsx
+++ src/main/webapp/src/App.tsx
@@ -2,14 +2,42 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import DirectoryForm from "./DirectoryForm";
+import { Navbar, Container, NavDropdown, Nav, Row, Col } from 'react-bootstrap';
 
 function App() {
     return (
         <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo"/>
-            </header>
-            <DirectoryForm></DirectoryForm>
+            <Navbar bg="light" expand="lg">
+                <Container>
+                    <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                    <Navbar.Collapse id="basic-navbar-nav">
+                        <Nav className="me-auto">
+                            <Nav.Link href="#home">Home</Nav.Link>
+                            <Nav.Link href="#link">Link</Nav.Link>
+                            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+                                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                                <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+                                <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                                <NavDropdown.Divider />
+                                <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+                            </NavDropdown>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+            <Container fluid>
+                <Row className="content">
+                    <Col sm={3} className="sidenav">Side nav</Col>
+                    <Col sm={9} className="center">
+                        <DirectoryForm></DirectoryForm>
+                    </Col>
+                </Row>
+            </Container>
+            <Container fluid className="footer">
+                Footer
+            </Container>
+
         </div>
     );
 }
--- src/main/webapp/src/DirectoryForm.tsx
+++ src/main/webapp/src/DirectoryForm.tsx
@@ -1,44 +1,36 @@
 import React from "react";
-import {Button, Form, InputGroup} from "react-bootstrap";
+import { Button, Form } from "react-bootstrap";
 import './DirectoryForm.css'
 
-type Props = {}
+function DirectoryForm() {
+    const [dir, setDir] = React.useState('')
 
-type State = {
-    directory: string;
-};
-
-class DirectoryForm extends React.Component<Props, State> {
-    constructor(props: Props) {
-        super(props);
-        this.state = {directory: ''};
-    }
-
-    //TODO binder sur le Input du form onChange={this.onChange}
-    onChange = (e: React.FormEvent<HTMLInputElement>): void => {
-        this.setState({directory: e.currentTarget.value});
+    const onDirChange = (e: React.FormEvent<HTMLInputElement>) => {
+        setDir(e.currentTarget.value)
     };
 
-    onSubmit = (e: React.FormEvent<HTMLButtonElement>): void => {
-        console.log("Let's call the API with "+this.state.directory)
+    const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        alert(`Selected dir : '${dir}'.`)
     };
 
-    render() {
-        return (
-            <div className="DirectoryForm">
-                <Form>
-                    <Form.Group className="mb-3" controlId="formBasicEmail">
-                        <Form.Label>Directory</Form.Label>
-                        <Form.Control type="text" placeholder="/Users/renaud/Downloads"/>
-                        <Form.Text className="text-muted">
-                            This directory will be scanned.
-                        </Form.Text>
-                    </Form.Group>
-                    <Button type="submit" onSubmit={this.onSubmit}>Submit</Button>
-                </Form>
-            </div>
-        );
-    }
+    return (
+        <div className="DirectoryForm">
+            <Form onSubmit={onSubmit}>
+                <Form.Group controlId="formBasicEmail">
+                    <Form.Label>Directory</Form.Label>
+                    <Form.Control
+                        type="text"
+                        placeholder="/Users/renaud/Downloads"
+                        onInput={onDirChange}
+                    />
+                    <Form.Text>This directory will be scanned.</Form.Text>
+                </Form.Group>
+                <Button type="submit">Submit</Button>
+            </Form>
+        </div>
+    );
+
 }
 
 export default DirectoryForm;
